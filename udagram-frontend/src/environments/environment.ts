30b7c5b4e2308b2175962decd2ce1ad037f3f1a8
Devoteng1 <lovebergers@gmail.com>
2022-09-07 01:22:47 +0000

    final

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad83d6f25a1d141fd855e8d026b30488-1364209769.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
